d1bf622d61e0d99e19d1dc471d4ceca2c7d20864
Sergey Skrynnikov <minotaar.hh@gmail.com>
2014-11-13 21:05:56 +0300

    added ui-router states

--- client/app.js
+++ client/app.js
@@ -1,4 +1,16 @@
 'use strict';
 
 angular.module('main', ['btford.socket-io'])
+    .config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise('/');
+        $stateProvider
+            .state('lobby', {
+                url: '/',
+                templateUrl: 'templates/lobby.html'
+            })
+            .state('game', {
+                url: '/game',
+                templateUrl: 'templates/game.html'
+            });
+    }])
     .run(['lobbySocket', function (lobbySocket) {}]);
